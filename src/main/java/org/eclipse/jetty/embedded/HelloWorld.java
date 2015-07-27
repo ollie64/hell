03b9609ec3c7c6d2971e7cb5e600881f55c95dff
Oleg Baskakov <obaskakov@collective.com>
2015-07-27 16:17:08 -0400

    none

--- src/main/java/org/eclipse/jetty/embedded/HelloWorld.java
+++ src/main/java/org/eclipse/jetty/embedded/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld extends AbstractHandler {
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html; charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println("<h1>Go to XXX!!</h1>");
         baseRequest.setHandled(true);
     }
 
